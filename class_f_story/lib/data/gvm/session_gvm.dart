c9217f52a1860bd84fe36c0dfb4009467ec798f0
SeoHong23 <ghd2408@naver.com>
2025-02-05 11:24:00 +0900

    블로그 만드릭

--- class_f_story/lib/data/gvm/session_gvm.dart
+++ class_f_story/lib/data/gvm/session_gvm.dart
@@ -5,7 +5,6 @@ import 'package:class_f_story/_core/utils/my_http.dart';
 import 'package:class_f_story/data/model/session_user.dart';
 import 'package:class_f_story/data/repository/user_repository.dart';
 import 'package:class_f_story/main.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -112,8 +111,93 @@ class SessionGVM extends Notifier<SessionUser> {
       ExceptionHandler.handleException('서버 연결 실패', stackTrace);
     }
   }
+
   // 로그아웃 행위
+  // 1. 디바이스 기기에 토큰 삭제
+  // 2. 나의 상태 갱신 -> SessionUser()
+  // 3. dio 전역 객체 헤더 토큰 제거 ==> '' 빈 문자열 처리
+  // 4. 화면 다 파괴하고 LoginPage 페이지 이동 처리
+  Future<void> logout() async {
+    try {
+      // 1
+      await secureStorage.delete(key: "accessToken");
+      // 2
+      state = SessionUser(
+        id: null,
+        username: null,
+        accessToken: null,
+        isLogin: false,
+      );
+      // 3
+      dio.options.headers["Authorization"] = '';
+      // 4
+      Navigator.pushNamedAndRemoveUntil(
+        mContext,
+        '/login',
+        (route) => false,
+      ); // 다 파괴하는거
+    } catch (e, stackTrace) {
+      ExceptionHandler.handleException('로그아웃 중 오류 발생', stackTrace);
+    }
+  }
+
   // 자동로그인 행위
+
+  // 로직 정리
+  // 0. 예외 처리
+  // 1. 디바이스 기기에 토큰 확인
+  // 2. 디바이스 토큰 유무 확인
+  // 3. 토큰 유효성 검사 ()
+  // 4. SessionUser 상태 갱신
+  // 5. dio 헤더에 토큰 다시 설정
+  // 6. 게시글 목록 페이지 이동 처리
+
+  Future<void> autoLogin() async {
+    try {
+      // 비동기 처리 --> 코드가 내려가지 않고 완료 될 때 까지 대기
+      // 1. 토큰을 디바이스에서 가져오기
+      // 2. JWT 토큰 유무 확인 (없을 경우, 있을 경우 )
+      String? accessToken = await secureStorage.read(key: 'accessToken');
+      if (accessToken == null) {
+        Navigator.popAndPushNamed(mContext, '/login');
+        return; // 실행의 제어권 반납한다.
+      }
+
+      // 토큰이 있다면 user_repository
+      // success --> true, false 로직 설계
+      Map<String, dynamic> resBody =
+          await userRepository.loginWithToken(accessToken);
+
+      // success --> false
+      // 서부 내부적으로 오류로 판결 처리
+      if (resBody['success']) {
+        Navigator.popAndPushNamed(mContext, '/login');
+        return; // 실행의 제어권 반납
+      }
+
+      // success --> true
+      // 상태 값을 변경 할 때 불변 객체를 사용하자.(깊은 복사 처리)
+      // 새로운 객체를 생성해서 넣자.
+      // 다운 캐스팅
+      Map<String, dynamic> data = resBody['response'];
+      state = SessionUser(
+        id: data['id'],
+        username: data['username'],
+        accessToken: accessToken,
+        isLogin: true,
+      );
+      // 상태 변경 완료
+      // 혹시 ... dio 헤더에 accessToken 을 다시 설정하자.
+      dio.options.headers['Authorization'] = accessToken;
+
+      // 게시글 목록 화면으로 이동 처리
+      Navigator.popAndPushNamed(mContext, '/post/list');
+    } catch (e, stackTrace) {
+      ExceptionHandler.handleException('자동 로그인 중 오류 발생', stackTrace);
+      // 화면 파괴하면서 이동 처리
+      Navigator.popAndPushNamed(mContext, '/login');
+    }
+  }
 }
 
 // 창고 관리자 선언 (창고 = 뷰 모델), 어느 창고 관리해라 지정!!
